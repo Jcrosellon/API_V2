9df49017ee4ee83488fa0a6cc6b4fe6c47e2c76c
Jcrosellon <119710206+Jcrosellon@users.noreply.github.com>
2024-06-20 00:52:04 -0500

    Nuevo cambio

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,21 +1,29 @@
-
-import userModel from '../models/user.model.js';
+import bcryptjs from 'bcryptjs';
 import { faker } from '@faker-js/faker';
+import jwt from 'jsonwebtoken';
+import userModel from '../models/user.model.js';
+
+
 export const createUser = async (req, res) => {
     try {
         await userModel.sync();
+        const salt= await bcryptjs.genSalt(10);
         const dataUser = req.body;
+        const passwordHash=await bcryptjs.hash(dataUser.user_password,salt);
         const createUser = await userModel.create({
-            user_user: dataUser.user_name,
-            user_password: dataUser.user_password,
+            user_user: dataUser.user_user,
+            user_password: passwordHash,
             userStatus_FK: dataUser.status,
-            role_fk: dataUser.role,
+            role_FK: dataUser.role,
+
         });
+        const token = jwt.sign({ email: createUser.user_user }, process.env.JWK_SECRET, { expiresIn: "1h" });
         res.status(201).json({
             ok: true,
             status: 201,
             message: 'Create User',
-            id: createUser.user_id
+            id: createUser.user_id,
+            token:token
         });
     } catch (error) {
         return res.status(500).json({
@@ -134,6 +142,45 @@ export const createUserFK = async (req, res) => {
     } catch (error) {
         return res.status(500).json({
             message: 'Something went wrong in the consultation',
+            status: 500,
+        });
+    }
+};
+
+export const loginUser = async (req, res) => {
+    try {
+        await userModel.sync();
+        const {email,password} = req.body;
+
+        if(!email || !password){
+            return res.status(400).json({error:"Missing required fields: email, password"});
+        }
+
+        const user = await userModel.findOne({
+            where: {
+                user_user: email,
+            }
+        });
+        if(!user){
+            return res.status(400).json({error:"User not found"});
+        }
+
+        const isMatch= await bcryptjs.compare(password,user.user_password);
+        if(!isMatch){
+            return res.status(400).json({error:"Invalid credentials"});
+        }
+        const token = jwt.sign({ email: user.user_user }, process.env.JWK_SECRET, { expiresIn: "1h" });
+
+        res.status(200).json({
+            ok: true,
+            status: 200,
+            message: 'Login API :)',
+            id: user.user_id,
+            token:token
+        });
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Something went wrong in the request',
             status: 500,
         });
     }
